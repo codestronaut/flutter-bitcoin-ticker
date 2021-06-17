767b80ecf2f47cf2cdad54844efdff102489d3df
adityarohman <hello.adityarohman@gmail.com>
2021-06-17 15:05:57 +0800

    feat: add platform env for widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+
 import 'package:bitcoin_ticker/data/coin_data.dart';
 import 'package:bitcoin_ticker/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,8 +16,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String selectedCurrency = 'USD';
 
+  DropdownButton<String> _getAndroidCurrencyPicker() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (var currency in currenciesList) {
+      dropdownItems.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+
+    return DropdownButton(
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value!;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker _getIosCurrencyPicker() {
+    List<Text> pirckerItems = [];
+    for (var currency in currenciesList) {
+      pirckerItems.add(
+        Text(currency),
+      );
+    }
+
+    return CupertinoPicker(
+      itemExtent: 36.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+      },
+      children: pirckerItems,
+    );
+  }
+
+  Widget getPicker() {
+    if (Platform.isAndroid) {
+      return _getAndroidCurrencyPicker();
+    }
+    return _getIosCurrencyPicker();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(selectedCurrency);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -49,15 +100,7 @@ class _HomePageState extends State<HomePage> {
                 bottom: 32.0,
               ),
               color: Colors.cyan,
-              child: CupertinoPicker(
-                itemExtent: 32.0,
-                onSelectedItemChanged: (selectedIndex) {
-                  print(selectedIndex);
-                },
-                children: [
-                  for (var currency in currenciesList) Text(currency),
-                ],
-              ),
+              child: getPicker(),
             ),
           ],
         ),
